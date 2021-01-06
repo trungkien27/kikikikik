810a1b3412dbed697cad7ed3a8641fd41efa765a
trungkien27 <75190172+trungkien27@users.noreply.github.com>
2021-01-06 10:32:18 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Thông tin cá nhân</title>
+</head>
+<body style=" background-image: url('https://cdn.tgdd.vn/Files/2019/09/26/1202150/d89f0a8587115e23c8156aa36db9ccbe5b67fd2813ff0_1280x719-800-resize.jpg'); background-repeat: no-repeat; background-size: cover;">
+	<a href="https://www.facebook.com/profile.php?id=100031941733125"><h1 title="Nguyên Trung kiên">Họ tên :Nguyên Trung Kiên </h1></a>
+	<a href="https://www.google.com/maps/d/viewer?ie=UTF8&oe=UTF8&msa=0&mid=1A-csFaOkCWBSRY607T0tgN1dafA&ll=21.230767278186626%2C105.64168250000002&z=14"><h2>Địa chỉ:lý nhân phú xuân bình xuyên vĩnh phúc</h2></a>
+	<h2 title="kien.nt.955@aptechlearning.edu.vn">Email:kien.nt.955@aptechlearning.edu.vn</h2>
+	<h2>Tuổi :18</h2>
+	<video controls>
+    <source src="https://v.pinimg.com/videos/mc/hls/7f/5b/a6/7f5ba69ef2bd6429c3424c81ef93defb.m3u8">
+    </video>
+
+	
+</body>
+</html>
